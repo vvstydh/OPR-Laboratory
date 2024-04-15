0c837ec03239c4744c31dbfe997b5f9de773eeeb
vvstydh <131537638+vvstydh@users.noreply.github.com>
2024-04-15 23:18:20 +0700

    Add files via upload

---
+++ OPR/OPR/laba1.1.cs
@@ -0,0 +1,102 @@
+﻿class Triangle
+{
+    private double ab, bc, ca, a, b, c, S, P, pP, Ha, Hb, Hc;
+
+    public bool newtriangle()
+    {
+        try
+        {
+            Console.WriteLine("Сторона ab");
+            ab = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона bc");
+            bc = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона ca");
+            ca = double.Parse(Console.ReadLine());
+            Console.WriteLine("Угол a");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Угол b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Угол c");
+            c = double.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Неверный формат");
+        }
+        if (ab < 0 || bc < 0 || ca < 0 || a < 0 || b < 0 || c < 0)
+        {
+            Console.WriteLine("Нельзя отрицательные!");
+            ab = 0;
+            bc = 0;
+            ca = 0;
+            a = 0;
+            b = 0;
+            c = 0;
+            return false;
+        }
+        else if (ab == 0 || bc == 0 || ca == 0 || a == 0 || b == 0 || c == 0)
+        {
+            Console.WriteLine("Нельзя нулевые значения!");
+            ab = 0;
+            bc = 0;
+            ca = 0;
+            a = 0;
+            b = 0;
+            c = 0;
+            return false;
+        }
+        else if ((ab + bc > ca || ab + ca > bc || bc + ca > ab) && (a + b + c) == 180)
+        {
+            Console.WriteLine("Успешно! Такой треугольник существует");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Такого треугольника не существует");
+            ab = 0;
+            bc = 0;
+            ca = 0;
+            a = 0;
+            b = 0;
+            c = 0;
+            return false;
+        }
+    }
+    public double square()
+    {
+        pP = perimetr() / 2;
+        S = Math.Sqrt(pP * (pP - ab) * (pP - bc) * (pP - ca));
+        return S;
+    }
+    public double perimetr()
+    {
+        P = ab + bc + ca;
+        return P;
+    }
+    public double ha()
+    {
+        Ha = square() * 2 / ab;
+        return Ha;
+    }
+    public double hb()
+    {
+        Hb = square() * 2 / bc;
+        return Hb;
+    }
+    public double hc()
+    {
+        Hc = square() * 2 / ca;
+        return Hc;
+    }
+    public void type()
+    {
+        if (a == 90 || b == 90 || c == 90)
+            Console.WriteLine("Треугольник прямоугольный");
+        else if (a == b & b == c)
+            Console.WriteLine("Треугольник равносторонний");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Треугольник равнобедренный");
+        else
+            Console.WriteLine("Обычный треугольник");
+    }
+}
---
+++ OPR/OPR/laba2.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class doc
+{
+    public void create(string path)
+    {
+        using (StreamWriter fs = new StreamWriter(path)) { }
+    }
+    public void write(string path, string text)
+    {
+        using (StreamWriter fs = new StreamWriter(path))
+            fs.Write(text);
+    }
+    public void move(string path, string newpath)
+    {
+        FileInfo move = new FileInfo(path);
+        if (move.Exists)
+            move.MoveTo(newpath);
+    }
+    public void copy(string path, string newpath)
+    {
+        FileInfo move = new FileInfo(path);
+        if (move.Exists)
+            move.CopyTo(newpath, true);
+    }
+    public void del(string path)
+    {
+        FileInfo move = new FileInfo(path);
+        if (move.Exists)
+            move.Delete();
+    }
+}
+class crypto : doc
+{
+    private byte[] key = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    private byte[] iv = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    public void code(string path)
+    {
+        byte[] data1 = Encoding.UTF8.GetBytes(data(path));
+        using (SymmetricAlgorithm alg = Aes.Create())
+        using (ICryptoTransform cryptoTransform = alg.CreateEncryptor(key, iv))
+        using (FileStream st = new FileStream(path, FileMode.OpenOrCreate))
+        using (CryptoStream cr = new CryptoStream(st, cryptoTransform, CryptoStreamMode.Write))
+            cr.Write(data1, 0, data1.Length);
+    }
+    public void encode(string path, string newpath)
+    {
+        StreamReader sr = null;
+        using (SymmetricAlgorithm alg = Aes.Create())
+        using (ICryptoTransform cryptoTransform = alg.CreateDecryptor(key, iv))
+        using (FileStream st = new FileStream(path, FileMode.OpenOrCreate))
+        using (CryptoStream cr = new CryptoStream(st, cryptoTransform, CryptoStreamMode.Read))
+        {
+            sr = new StreamReader(cr);
+            write(newpath, sr.ReadToEnd());
+        }
+    }
+    private string data(string path)
+    {
+        using (StreamReader read = new StreamReader(path))
+            return read.ReadToEnd();
+    }
+}
