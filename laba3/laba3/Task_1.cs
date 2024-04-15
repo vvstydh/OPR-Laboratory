a6c21bc07be549b002004e44b85d0eda56480f85
vvstydh <131537638+vvstydh@users.noreply.github.com>
2024-04-15 23:19:46 +0700

    Add files via upload

---
+++ laba3/laba3/Task_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba3
+{
+    public partial class Task_1 : Form
+    {
+        public Task_1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v = double.Parse(V.Text);
+                double s = double.Parse(S.Text);
+                answer.Text = ($"Так вот какова высота цилиндра:\n {v / s}");
+            }
+            catch (FormatException) { answer.Text = ($"Прости, не верный формат"); }
+        }
+    }
+}
